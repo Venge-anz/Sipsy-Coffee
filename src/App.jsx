b8e2e545c059b5f49fa746d81f929a7d08e4ef25
Claudia Vivo Rosa <claudia@MacBook-Air-de-Claudia.local>
2025-10-03 22:48:31 +0200

    cosas hechas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { Home } from "./Home/Home";
+import { Route, Routes } from "react-router";
+import { Home } from "./Home/Home/";
+import { Login } from "./Auth/Login";
 
 export const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/*" element={<Home />} />
+
+      {/* Rutas restantes */}
+
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 };
